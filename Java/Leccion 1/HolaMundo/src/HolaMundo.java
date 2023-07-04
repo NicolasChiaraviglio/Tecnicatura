6b3f38fbbee262841bdfd6d8ea924decb4629690
Nicolas <nicolaschiaraviglio70@gmail.com>
2023-07-03 21:24:50 -0300

    Clase9

---
+++ Java/Leccion 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,75 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Mega Tecnologia
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        /*System.out.println("Hola mundo desde java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+         */
+
+        //Var - inferencia de tipos en java
+        /*var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+         */
+        //soutv + tab
+        //para ejecutar shift + f6
+        //Ejercicio de Concatenacion
+        /*var usuario = "Nicolas";
+        var titulo = "Estudiante";
+        var union = usuario + " " + titulo;
+        System.out.println("union = " + union);
+
+        var a = 8;
+        var b = 4;
+        System.out.println(a + b);
+        System.out.println(usuario + a + b);
+        System.out.println(usuario + (a + b));
+
+        //Caracteres Especiales
+        var nombre = "Natalia";
+        System.out.println("\nNueva linea: \n" + nombre);//\n salto de linea
+        System.out.println("Tabulacion: \t" + nombre);//\t espacio tabulado
+        System.out.println("\t\t.:Menu:.");//Ejemplo \t
+        System.out.println("Retroseso: \b" + nombre);//\b borra un espacio hacia atras
+        System.out.println("Comillas Simples: \'"+ nombre +"\'");//\' comillas simples
+        System.out.println("Comillas Dobles: \""+ nombre +"\"");//\" comillas dobles
+         */
+        //Clase Scanner
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el titulo: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("titulo2 = " + titulo2+" "+usuario2);
+        
+        
+        
+        
+        
+        
+        
+
+    }
+}
