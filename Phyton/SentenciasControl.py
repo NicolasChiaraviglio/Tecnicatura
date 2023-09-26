f6de2c713d2efc8ad7421f9432ba613a50ed5497
Nicolas Chiaraviglio <nicolaschiaraviglio70@gmail.com>
2023-09-26 20:00:46 -0300

    commiteando carpetas

---
+++ Phyton/SentenciasControl.py
@@ -0,0 +1,46 @@
+#IF/ELSE
+condicion = True
+if condicion == True:
+    print("Condicion Verdadera")
+elif condicion == False:
+    print("Condicion Falsa")
+else:
+    print("Condicion sin especifir")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
