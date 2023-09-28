6a661e2106bcda898f0e7a215058ed76d38ac7d6
Nicolas Chiaraviglio <nicolaschiaraviglio70@gmail.com>
2023-09-28 04:13:29 -0300

    Ciclos

--- Phyton/SentenciasControl.py
+++ Phyton/SentenciasControl.py
@@ -35,6 +35,65 @@ print(f"El resultado es: {resultado}")
 condicion = False
 print("Condicion Verdadera") if condicion else print ("Condicion Falsa")#no se debe utilizar elif#no se aconseja en codigos largos complejos
 
+#CICLO WHILE(MIENTRAS)#SE VA A REPETIR SIEMPRE Y CUANDO LA CONDIDICON SEA VERDADERA
+
+contador = 0
+while contador < 78:
+    print("Ejecutamos nuestro ciclo while", contador)
+    contador += 1
+else:
+    print("Fin del ciclo while")
+
+#EJERCICIOS CICLO WHILE1
+
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(f"Unidad numero: {contador}")
+    contador += 1
+
+#EJERCICIO CICLO WHILE2
+
+minimo = 1
+contador = 5
+while contador >= minimo:
+    print(f"Unidad numero: {contador}")
+    contador -= 1
+else:
+    print("FIN DEL CONTEO.")
+
+#CICLO FOR(PARA)#NOS PERMITE ITERAR(RECORRER) UNA LISTA DE DATOS O ELEMENTOS
+
+cadena = "HELLO"
+for letra in cadena:
+    print(letra)
+else:
+    print("Fin del ciclo for")
+
+#PALABRA RESERVADA BREAK
+
+for letra in "Alemania":
+    if letra == "a":
+        print(f"Letra encontrada: {letra}")
+        break#break rompe el ciclo y termina, sin siquiera llegar al else
+else:
+    print("Fin del ciclo for")
+
+#PALABRA RESERVADA CONTINUE
+
+for i in range(6):#rango de numeros, sin incluir el 6 en este caso, ya que toma 6 digitos desde el 0
+    if i % 2 == 0:
+        print(f"Valor: {i}")
+
+for i in range(6):
+    if i % 2 != 0:#cuando el resto de la divicion por 2 sea DISTINTA de 0
+        continue#eludir o anular todos los numeros que sean impares, en este caso
+    print(f"Valor: {i}")
+
+
+
+
+
 
 
 
