93a6512fb4655803886cec961b301374557fd9f0
Nicolas <nicolaschiaraviglio70@gmail.com>
2023-11-01 19:09:39 -0300

    Leccion 2

---
+++ Java/Leccion2/CicloWhile/src/main/java/CicloWhile/EjercicioWhile01.java
@@ -0,0 +1,15 @@
+package CicloWhile;
+
+public class EjercicioWhile01 {
+
+    public static void main(String[] args) {
+
+        var conteo = 0;//var = inferencia de tipos
+        while (conteo < 7) {
+            System.out.println("conteo = " + conteo);
+            conteo++;         
+        }
+    }
+    
+
+}
